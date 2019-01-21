acfc079f7a14f6bd9682f50d0ebb892acd272c9c
Vassilis <vas.terzo@gmail.com>
2019-01-21 08:30:47 +0200

    add images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,14 @@
 import React from 'react'
-// import { Link } from 'gatsby'
-import Logo from '../images/codefully2.png'
-import Banner from '../images/2030.jpg'
-
+import Logo from '../images/codefully2i.png'
 import Layout from '../components/layout'
-// import Image from '../components/image'
 import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 1600,
-        padding: `1rem 1rem 0`,
-      }}
-    >
-      <img
-        alt="codefully.io"
-        src={Logo}
-        style={{ marginLeft: 120, maxWidth: 300 }}
-      />
-      <img
-        alt="banner"
-        src={Banner}
-        style={{ marginTop: -170, zIndex: -1, position: 'relative', maxWidth: '100%' }}
-      />
-    </div>
-    <div style={{ backgroundColor: '#000', color: '#FFF', padding: '5rem 0' }}>
+    <div className="banner1">
       <div style={{ maxWidth: 1000, margin: '0 auto' }}>
+        <img alt="codefully.io" src={Logo} style={{ maxWidth: 300 }} />
         <p
           style={{
             fontSize: '2.3em',
@@ -37,10 +16,11 @@ const IndexPage = () => (
             fontWeight: 'bold',
           }}
         >
-          We transform stories into applications that engage people and engaged
-          people are inspired, fulfilled and create better results. Our work
-          spans industries and continents, seeking to bring our clients' ideas,
-          systems and stories to life.
+          We are a lean team of senior software engineers. We add value to
+          whatever we get involved. We transform stories into applications that
+          engage people and engaged people are inspired, fulfilled and create
+          better results. Our work spans industries and continents, seeking to
+          bring our clients' ideas, systems and stories to life.
         </p>
         <p
           style={{
